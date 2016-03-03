af33c993f6cb0c2ea6ddd1a3d2c4784cd9a12062
Deepak <deepak.262903@gmail.com>
2016-03-03 23:59:16 +0530

    Created demo app using AppRTC static library.

---
+++ AppRTCDemo/AppRTCDemo/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  AppRTCDemo
+//
+//  Created by Deepak on 03/03/16.
+//  Copyright © 2016 Deepak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RTCPeerConnectionFactory.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
